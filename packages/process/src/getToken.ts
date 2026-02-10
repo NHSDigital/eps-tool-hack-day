44f6353734059302cc8106124264088e0e5bf2c2
Anthony Brown <anthony.brown8@nhs.net>
2026-02-10 11:58:56 +0000

    add auth

---
+++ packages/process/src/getToken.ts
@@ -0,0 +1,214 @@
+import axios, {AxiosInstance, AxiosResponse} from "axios"
+import {JSDOM} from "jsdom"
+import {wrapper} from "axios-cookiejar-support"
+import {CookieJar} from "tough-cookie"
+
+type EnvironmentSecrets = {
+  clientId: string
+  clientSecret: string
+}
+
+export class AuthClient {
+  private readonly environment: string
+  readonly clientId: string
+  readonly clientSecret: string
+  readonly callbackUrl: string
+  private readonly state: string
+
+  constructor() {
+    const {clientId, clientSecret} = this.getApiCredentials()
+    this.clientId = clientId
+    this.clientSecret = clientSecret
+
+    this.environment = "internal-dev"
+    this.callbackUrl = "https://google.com" // using /callback causes the website to return a 404, which upsets axios
+    this.state = "1234567890"
+  }
+
+  getBaseUrl(): string {
+    return `https://${this.environment}.api.service.nhs.uk/oauth2-mock`
+  }
+
+  private getApiCredentials = (): EnvironmentSecrets => {
+    const clientId = process.env["API_CLIENT_ID"]
+    const clientSecret = process.env["API_CLIENT_SECRET"]
+
+    return {
+      clientId,
+      clientSecret
+    }
+  }
+
+  getAuthParams(): Record<string, string> {
+    return {
+      client_id: this.clientId,
+      redirect_uri: this.callbackUrl,
+      state: this.state,
+      response_type: "code"
+    }
+  }
+
+  // https://internal-dev.api.service.nhs.uk/oauth2-mock/authorize
+  // ?client_id=9AfEOqUltvbzj8YKXPZmN1ZfwaCRo4hs&redirect_uri=https://example.org/callback&state=123&response_type=code
+  getAuthUrl(): string {
+    return `/authorize`
+  }
+
+  getTokenUrl(): string {
+    return `/token`
+  }
+}
+
+export const getAuthForm = async (axiosInstance: AxiosInstance, requestUrl: string): Promise<AxiosResponse> => {
+  const response = await axiosInstance.get(requestUrl)
+
+  if (response.status !== 200) {
+    throw new Error(`Cannot retrieve auth token: ${requestUrl} returned ${response.data}`)
+  }
+
+  return response
+}
+
+type FormInputs = {
+  username?: string
+  login?: string
+}
+
+type FormModel = {
+  action: string
+  method: string
+  data: FormInputs
+}
+
+export const parseAuthForm = (htmlForm: string): FormModel => {
+  const {document} = new JSDOM(htmlForm).window
+  const form = document.getElementById("kc-form-login")
+
+  const formAction = form.getAttribute("action")
+  const formMethod = form.getAttribute("method")
+  const formInputs = form.getElementsByTagName("input")
+
+  const inputs: FormInputs = {}
+  for (const item of formInputs) {
+    const name: string = item.getAttribute("name")
+    const value: string = item.getAttribute("value")
+    inputs[name] = value
+  }
+
+  // Set username with test user value, e.g. 656005750108
+  inputs["username"] = "656005750108"
+
+  return {
+    action: formAction,
+    method: formMethod,
+    data: inputs
+  }
+}
+
+/**
+ *
+ * @param url - the url to the /token endpoint
+ * @param formData - key value map of the form inputs
+ * @returns Apigee auth token
+ */
+export const sendAuthForm = async (axiosInstance: AxiosInstance, form: FormModel): Promise<AxiosResponse> => {
+  const response = await axiosInstance.post(
+    form.action,
+    form.data,
+    // {username: "656005750108"},
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    }
+  )
+
+  //NOSONAR
+  // if (result.status !== 200) throw `Could not retrieve token: ${result.data}`
+
+  return response
+}
+
+export const getAxiosInstance = (client: AuthClient): AxiosInstance => {
+  const jar = new CookieJar()
+  const axiosInstance = wrapper(
+    axios.create({
+      jar,
+      // withCredentials: true,
+      baseURL: client.getBaseUrl()
+    })
+  )
+
+  return axiosInstance
+}
+
+type AuthCallbackResponseData = {
+  code: string
+  state: string
+}
+
+const parseAuthCallbackResponse = (authResponse: AxiosResponse): AuthCallbackResponseData => {
+  const responsePath: string = authResponse.request.path // /?code=UmsJVKNA&state=1234567890
+
+  const searchParams = new URLSearchParams(responsePath.substring(2)) // skip the leading `/?`
+  return {
+    code: searchParams.get("code"),
+    state: searchParams.get("state")
+  }
+}
+
+type TokenResponse = {
+  access_token: string
+  expires_in: string
+  refresh_token: string
+  refresh_token_expires_in: string
+  refresh_count: string
+  token_type: string
+  sid: string
+}
+
+const exchangeCodeForToken = async (
+  axiosClient: AxiosInstance,
+  authClient: AuthClient,
+  authCallbackData: AuthCallbackResponseData
+): Promise<TokenResponse> => {
+  const data = {
+    grant_type: "authorization_code",
+    code: authCallbackData.code,
+    client_id: authClient.clientId,
+    client_secret: authClient.clientSecret,
+    redirect_uri: authClient.callbackUrl
+  }
+
+  const tokenResponse = await axiosClient.post(authClient.getTokenUrl(), data, {
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    }
+  })
+
+  return tokenResponse.data
+}
+
+export const getAuthToken = async (): Promise<string> => {
+  const client = new AuthClient()
+  const axiosInstance = getAxiosInstance(client)
+
+  const query = Object.entries(client.getAuthParams())
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&")
+  const authPath = `${client.getAuthUrl()}?${query}`
+
+  const authFormResponse = await getAuthForm(axiosInstance, authPath)
+  const authFormData = parseAuthForm(authFormResponse.data)
+
+  const authFormSubmissionResponse = await sendAuthForm(axiosInstance, authFormData)
+  const callbackData = parseAuthCallbackResponse(authFormSubmissionResponse)
+
+  const tokenResponse = await exchangeCodeForToken(axiosInstance, client, callbackData)
+
+  return tokenResponse.access_token
+}
+
+export async function getToken(): Promise<string> {
+  return getAuthToken()
+}
---
+++ packages/process/src/util.ts
@@ -0,0 +1,127 @@
+import _epsPrepare from "./prepare.json"
+import { getAuthToken } from "./getToken"
+
+const FHIR_BASE_URL = "https://internal-dev.api.service.nhs.uk/fhir-prescribing"
+
+type FhirClient = {
+  post: (path: string, body: unknown) => Promise<unknown>
+}
+
+export async function call_fhir(): Promise<FhirClient> {
+  const token = await getAuthToken()
+
+  return {
+    post: async (path: string, body: unknown) => {
+      const response = await fetch(`${FHIR_BASE_URL}${path}`, {
+        method: "POST",
+        headers: {
+          Authorization: token,
+          "content-type": "application/json"
+        },
+        body: JSON.stringify(body)
+      })
+
+      if (!response.ok) {
+        const errorText = await response.text()
+        throw new Error(`FHIR request failed: ${response.status} ${response.statusText} ${errorText}`)
+      }
+
+      return response.json()
+    }
+  }
+}
+
+export function shortPrescriptionId() {
+  const _PRESC_CHECKDIGIT_VALUES = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ+"
+  const hexString = crypto.randomUUID().replace(/-/g, "").toUpperCase()
+  const first = hexString.substring(0, 6)
+  const middle ="A12345"
+  const last = hexString.substring(12, 17)
+  let prescriptionID = `${first}-${middle}-${last}`
+  const prscID = prescriptionID.replace(/-/g, "")
+  const prscIDLength = prscID.length
+  let runningTotal = 0
+  const strings = prscID.split("")
+  strings.forEach((character, index) => {
+    runningTotal = runningTotal + parseInt(character, 36) * 2 ** (prscIDLength - index)
+  })
+  const checkValue = (38 - (runningTotal % 37)) % 37
+  const checkDigit = _PRESC_CHECKDIGIT_VALUES.substring(checkValue, checkValue + 1)
+  prescriptionID += checkDigit
+  return prescriptionID
+}
+
+export function getPrepareTemplate() {
+  return JSON.parse(JSON.stringify(_epsPrepare))
+}
+
+function setBundleIdAndValue(data: any, resourceType = "others") {
+  const identifierValue = crypto.randomUUID()
+  data.id = crypto.randomUUID()
+  if (resourceType === "claim") {
+    data.identifier[0].value = identifierValue
+  } else {
+    data.identifier.value = identifierValue
+  }
+  return identifierValue
+}
+
+function updateMessageHeader(entry, site) {
+  if (entry.resource.resourceType === "MessageHeader") {
+    entry.fullUrl = "urn:uuid:" + crypto.randomUUID()
+    entry.resource.destination[0].receiver.identifier.value = site
+  }
+}
+
+export async function preparePrescription(numberToCreate: number) {
+  let position = 2
+  let resp = null
+  const site = "A83008"
+
+  for (let i = 0; i < numberToCreate; i++) {
+    const now = new Date()
+    const later = new Date()
+    later.setMonth(later.getMonth() + 3)
+
+    const validStart = `${now.getFullYear()}-${("0" + (now.getMonth() + 1)).slice(-2)}-${("0" + now.getDate()).slice(-2)}`
+    const validEnd = `${later.getFullYear()}-${("0" + (later.getMonth() + 1)).slice(-2)}-${("0" + later.getDate()).slice(-2)}`
+    const shortPrescId = shortPrescriptionId()
+    const longPrescId = crypto.randomUUID()
+    let addRefId = false
+
+    console.log(shortPrescId)
+    const data = getPrepareTemplate()
+
+    const identifierValue = setBundleIdAndValue(data, "others")
+
+
+    for (const entry of data.entry) {
+      if (entry.resource.resourceType === "MedicationRequest") {
+
+        entry.resource.groupIdentifier.extension[0].valueIdentifier.value = longPrescId
+        entry.resource.groupIdentifier.value = shortPrescId
+        entry.resource.dispenseRequest.validityPeriod.start = validStart
+        entry.resource.dispenseRequest.validityPeriod.end = validEnd
+        entry.resource.dispenseRequest.performer.identifier.value = site
+
+      }
+      updateMessageHeader(entry, site)
+    }
+
+    const preparedPrescription = new Map()
+    preparedPrescription.set("shortPrescId", shortPrescId)
+    preparedPrescription.set("longPrescId", longPrescId)
+    preparedPrescription.set("identifierValue", identifierValue)
+    preparedPrescription.set("prepareRequest", data)
+    preparedPrescription.set("addRefId", addRefId)
+
+    await (await call_fhir())
+      .post("/FHIR/R4/$prepare", data)
+      .then((_data) => {
+        resp = _data
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}

3470e945561827dfdbe7217d748df7dc185ed842
Anthony Brown <anthony.brown8@nhs.net>
2026-02-10 10:23:15 +0000

    add uuid

--- packages/process/src/handler.ts
+++ packages/process/src/handler.ts
@@ -3,23 +3,29 @@ import {injectLambdaContext} from "@aws-lambda-powertools/logger/middleware"
 import middy from "@middy/core"
 import inputOutputLogger from "@middy/input-output-logger"
 import errorHandler from "@nhs/fhir-middy-error-handler"
+import crypto from "crypto"
 
-const logger = new Logger({serviceName: "status"})
+const logger = new Logger({serviceName: "process"})
 
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} _event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
+const generateUuid = (): string => {
+  if (typeof crypto.randomUUID === "function") {
+    return crypto.randomUUID()
+  }
+
+  const bytes = crypto.randomBytes(16)
+  bytes[6] = (bytes[6] & 0x0f) | 0x40
+  bytes[8] = (bytes[8] & 0x3f) | 0x80
+  const hex = bytes.toString("hex")
+  return `${hex.slice(0, 8)}-${hex.slice(8, 12)}-${hex.slice(12, 16)}-${hex.slice(16, 20)}-${hex.slice(20)}`
+}
+
 
 const lambdaHandler = async (event: any): Promise<any> => {
+  const requestId = event.headers["x-request-id"] ?? generateUuid()
+
   logger.appendKeys({
     "nhsd-correlation-id": event.headers["nhsd-correlation-id"],
-    "x-request-id": event.headers["x-request-id"],
+    "x-request-id": requestId,
     "nhsd-request-id": event.headers["nhsd-request-id"],
     "x-correlation-id": event.headers["x-correlation-id"],
     "apigw-request-id": event.requestContext.requestId

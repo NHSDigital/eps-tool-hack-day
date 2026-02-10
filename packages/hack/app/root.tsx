dfdecd6eec44d966c73e2a83ff6f6ffc92d02c86
Bence Gadanyi <bence.gadanyi1@nhs.net>
2026-02-10 11:39:39 +0000

    chore: base site

--- packages/hack/app/root.tsx
+++ packages/hack/app/root.tsx
@@ -30,6 +30,15 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if (window.location.pathname === "/site") {
+                window.location.pathname = "/site/";
+              }
+            `,
+          }}
+        />
         <Meta />
         <Links />
       </head>

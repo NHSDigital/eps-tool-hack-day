ea79eeaac61e6055f4ade54953f89aa61fc42746
Bence Gadanyi <bence.gadanyi1@nhs.net>
2026-02-10 11:19:08 +0000

    chore: styling

--- packages/hack/app/root.tsx
+++ packages/hack/app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import "nhsuk-frontend/dist/nhsuk.css";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
--- packages/hack/app/routes/home.tsx
+++ packages/hack/app/routes/home.tsx
@@ -1,12 +1,35 @@
-import { Welcome } from "../welcome/welcome";
+import { Header, Container, Button } from "nhsuk-react-components";
 
 export function meta() {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "NHS EPS Tool" },
+    { name: "description", content: "Create a prescription" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      {/* @ts-expect-error - Header types mismatch with React 19 */}
+      <Header>
+        <Header.Container>
+          <div className="nhsuk-header__logo">
+            <a className="nhsuk-header__link" href="/" aria-label="NHS homepage">
+              <img src="/site/nhs-rebranded.png" alt="NHS Rebranded" style={{ height: "50px" }} />
+            </a>
+          </div>
+        </Header.Container>
+      </Header>
+      <Container>
+        <main className="nhsuk-main-wrapper" id="maincontent" role="main">
+          <div className="nhsuk-grid-row">
+            <div className="nhsuk-grid-column-two-thirds">
+              <h1>EPS Tool Hack Day</h1>
+              <Button href="/create-prescription">Create a prescription</Button>
+            </div>
+          </div>
+        </main>
+      </Container>
+    </>
+  );
 }

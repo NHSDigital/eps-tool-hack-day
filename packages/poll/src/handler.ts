322d953a55f7f698f8ebba865e2ebc7cd7be7d1a
Jim Wild <james.wild6@nhs.net>
2026-02-10 11:44:13 +0000

    Add the handler for the poll lambda

---
+++ packages/poll/src/handler.ts
@@ -0,0 +1,44 @@
+import {Logger} from "@aws-lambda-powertools/logger"
+import {injectLambdaContext} from "@aws-lambda-powertools/logger/middleware"
+import middy from "@middy/core"
+import inputOutputLogger from "@middy/input-output-logger"
+import errorHandler from "@nhs/fhir-middy-error-handler"
+
+const logger = new Logger({serviceName: "poll"})
+
+
+const lambdaHandler = async (event: any): Promise<any> => {
+  logger.appendKeys({
+    "nhsd-correlation-id": event.headers["nhsd-correlation-id"],
+    "x-request-id": event.headers["x-request-id"],
+    "nhsd-request-id": event.headers["nhsd-request-id"],
+    "x-correlation-id": event.headers["x-correlation-id"],
+    "apigw-request-id": event.requestContext.requestId
+  })
+
+  const commitId = process.env.COMMIT_ID
+  const versionNumber = process.env.VERSION_NUMBER
+
+
+  const statusBody = {commitId: commitId, versionNumber: versionNumber}
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(statusBody),
+    headers: {
+      "Content-Type": "application/health+json",
+      "Cache-Control": "no-cache"
+    }
+  }
+}
+
+export const handler = middy(lambdaHandler)
+  .use(injectLambdaContext(logger, {clearState: true}))
+  .use(
+    inputOutputLogger({
+      logger: (request) => {
+        logger.info(request)
+      }
+    })
+  )
+  .use(errorHandler({logger: logger}))
